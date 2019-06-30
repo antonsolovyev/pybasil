356c6ccdb9c561b8aab5445917ebebdbf58aced7
Anton Solovyev <asolo@uber.com>
2019-06-30 15:53:51 -0600

    Spark

---
+++ pi_on_spark.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+from random import random
+from operator import add
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == '__main__':
+    """
+        Usage: pi [partitions]
+    """
+    spark = SparkSession.builder.master('spark://white.home:7077').appName("PI on Spark").getOrCreate()
+
+    partitions = 30
+    n = 1000000 * partitions
+
+    def f(_):
+        x = random() * 2 - 1
+        y = random() * 2 - 1
+        return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    print('PI is roughly %f' % (4.0 * count / n))
+
+    spark.stop()
